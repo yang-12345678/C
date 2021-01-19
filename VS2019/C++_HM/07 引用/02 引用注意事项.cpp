f31d36819833008a76f5d108b3b924dfef0bb8be
yang-12345678 <1958523752@qq.com>
2021-01-19 19:10:04 +0800

    2021.1.18 c++学习第四天

---
+++ VS2019/C++_HM/07 引用/02 引用注意事项.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a = 10;
+//
+//	// 1、引用必须初始化
+//	int& b = a;
+//	//int& b; // 错误
+//
+//	// 2、引用初始化后不可以改变
+//
+//	system("pause");
+//
+//	return 0;
+//}
---
+++ VS2019/C++_HM/07 引用/03 引用做函数参数.cpp
@@ -0,0 +1,25 @@
+//#include <iostream>
+//using namespace std;
+//
+//// 交换函数 (引用传值)
+//int mySwap(int& a, int& b)
+//{
+//	int temp = a;
+//	a = b;
+//	b = temp;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//
+//	mySwap(a, b);
+//
+//	cout << "a = " << a << endl;
+//	cout << "b = " << b << endl;
+//
+//	system("pause");
+//
+//	return 0;
+//}
