f4526fb9ea5f50996c05c94f67356213e8c56540
yang-12345678 <1958523752@qq.com>
2021-01-19 20:09:58 +0800

    2021.1.19 c++ 学习第五天

---
+++ VS2019/C++_HM/09 类和对象/02 封装案例.cpp
@@ -0,0 +1,46 @@
+//#include <iostream>
+//using namespace std;
+//
+//// 设计学生类
+//class Student
+//{
+//public:
+//	string name;  // 姓名
+//
+//	int id; // 学号
+//
+//	// 显示姓名和函数
+//	void show()
+//	{
+//		cout << "姓名：" << name << "  学号：" << id << endl;
+//	}
+//	
+//	// 给姓名赋值
+//	void setName(string ming)
+//	{
+//		name = ming;
+//	}
+//};
+//
+//int main()
+//{
+//	// 实例化
+//	Student s;
+//
+//	/*s.name = "张三";*/
+//	s.setName("张三");
+//	s.id = 1;
+//
+//	s.show();
+//
+//	Student s2;
+//
+//	s2.name = "李四";
+//	s2.id = 2;
+//
+//	s2.show();
+//
+//	system("pause");
+//
+//	return 0;
+//}
