a394493144806346b90dbc90b40401ac441b1aba
yang-12345678 <1958523752@qq.com>
2021-04-21 16:56:23 +0800

    。

---
+++ 算法设计/算法设计/02 例1 改.cpp
@@ -0,0 +1,23 @@
+// 算法时间复杂度： O(n)
+
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{   
+//    int i, n, sign;
+//    float s, t = 1;
+//    cout << "请输入n的值:";
+//    cin >> n;
+//    s = 1;
+//    sign = 1;
+//    for (i = 2; i <= n; i++)
+//    {
+//        sign = -sign;
+//        t = t * (2 * i - 2) * (2 * i - 1);
+//        s = s + sign / t;
+//    }
+//    cout << "sum=" << s << endl;
+//    system("pause");
+//    return 0;
+//}
