55f65a2275f73877e987a0d99642dc8dea0f5506
yang-12345678 <1958523752@qq.com>
2021-06-09 13:56:57 +0800

    。

--- C++_HM/01 C++/03 变量.cpp
+++ C++_HM/01 C++/03 变量.cpp
@@ -1,14 +1,18 @@
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	int a;
-//	a = 10;
-//
-//	cout << "a=" << a << endl;
-//
-//	system("pause");
-//
-//	return 0;
-//}
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	a = 10;
+	int b;
+	b = 10;
+
+	cout << &a << endl;
+	cout << &b << endl;
+	cout << "a=" << a << endl;
+
+	system("pause");
+
+	return 0;
+}
