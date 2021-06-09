ae97fd8a1cfd76ad5840bed13eba0ec02b8fd060
yang-12345678 <1958523752@qq.com>
2021-06-09 13:58:03 +0800

    。

--- C++_HM/04 函数/03 函数的分文件编写.cpp
+++ C++_HM/04 函数/03 函数的分文件编写.cpp
@@ -2,17 +2,7 @@
 #include "swap.h"
 using namespace std;
 
-//void swap(int a, int b);
-//
-//void swap(int a, int b)
-//{
-//	int temp = a;
-//	a = b;
-//	b = temp;
-//
-//	cout << "a = " << a << endl;
-//	cout << "b = " << b << endl;
-//}
+
 
 
 int main()
--- C++_HM/04 函数/swap.h
+++ C++_HM/04 函数/swap.h
@@ -1,2 +1,12 @@
+#include <iostream>
+using namespace std;
 
-void swap(int a, int b);
+void swap(int a, int b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+}
